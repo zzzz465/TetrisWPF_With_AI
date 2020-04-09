0654be8bbebf5ce6959464d8301f1206079536de
madeline <zzzz465@naver.com>
2020-04-09 16:06:16 +0900

    basic movement input processing

--- Tetris_With_Deep_Learning_AI/Tetris/TetrisGame.cs
+++ Tetris_With_Deep_Learning_AI/Tetris/TetrisGame.cs
@@ -37,25 +37,24 @@ namespace Tetris
         public void Update(uint curMilliSecond)
         {
             var currentState = inputProvider.GetInputState();
-
+            var mino = currentPiece.minoType;
             if(!currentState.isTrue(InputState.HardDrop))
             {
-                if(currentState.isTrue(InputState.CCW))
+                if(currentState.isTrue(InputState.CCW) || currentState.isTrue(InputState.CW))
                 {
-
-                }
-                else if(currentState.isTrue(InputState.CW))
-                {
-
+                    if(TrySpin(currentState, out var newOffsetPos))
+                        currentPiece.offset = newOffsetPos;
                 }
 
                 if(currentState.isTrue(InputState.LeftPressed))
                 {
-
+                    if(TryMove(new Point(-1, 0), out var newOffsetPos))
+                        currentPiece.offset = newOffsetPos;
                 }
                 else if(currentState.isTrue(InputState.RightPressed))
                 {
-
+                    if(TryMove(new Point(1, 0), out var newOffsetPos))
+                        currentPiece.offset = newOffsetPos;
                 }
                 
                 if(currentState.isTrue(InputState.SoftDrop))
@@ -67,6 +66,66 @@ namespace Tetris
             {
 
             }
+        }
+
+        bool TryMove(Point localMoveOffset, out Point newOffsetPos)
+        {
+            var newOffset = new Point(localMoveOffset.X + currentPiece.offset.X, localMoveOffset.Y + currentPiece.offset.Y);
+            var expectedPos = currentPiece.minoType.GetPos(newOffset, currentPiece.rotState);
+            var success = TryShift(currentPiece.GetPos(), expectedPos, currentPiece.minoType);
+
+            if(success)
+                newOffsetPos = newOffset;
+            else
+                new Point();
+            
+            return success;
+        }
+
+        bool TryShift(IEnumerable<Point> before, IEnumerable<Point> after, Tetromino mino)
+        {
+            var canMove = !after.Any(p => tetrisGrid.Get(p));
+            if(canMove)
+            {
+                tetrisGrid.Set(before, false);
+                tetrisGrid.Set(after, true, mino);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        bool TrySpin(InputState inputState, out Point newOffsetPos)
+        {
+            var mino = currentPiece.minoType;
+
+            if(inputState.isTrue(InputState.CCW) && inputState.isTrue(InputState.CW))
+                throw new InvalidOperationException("Cannot have CCW and CW at the same time!");
+
+            IEnumerable<Point> expectedLocalOffsetPos;
+
+            var before = currentPiece.rotState;
+            RotationState after;
+
+            if(inputState.isTrue(InputState.CCW))       after = currentPiece.rotState.CCW();
+            else if(inputState.isTrue(InputState.CW))   after = currentPiece.rotState.CW();
+            else                                        return false;
+
+            expectedLocalOffsetPos = SRS.Translation(currentPiece.minoType, before, after);
+
+            foreach(var offsetPos in expectedLocalOffsetPos)
+            {
+                var expectedPos = mino.GetPos(offsetPos, currentPiece.rotState.CCW());
+                bool success = TryShift(currentPiece.GetPos(), expectedPos, currentPiece.minoType);
+
+                if(success)
+                {
+                    newOffsetPos = offsetPos;
+                    return true;
+                }
+            }
+            newOffsetPos = new Point();
+            return false;
         }
     }
 }
