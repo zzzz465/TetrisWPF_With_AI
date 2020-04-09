cd42e746ffdf61953e85575f7a021cc673fe30be
madeline <zzzz465@naver.com>
2020-04-09 12:57:30 +0900

    Bag for tetromino provider

---
+++ Tetris_With_Deep_Learning_AI/Tetris/Bag.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tetris
+{
+    public class TetrominoBag
+    {
+        Queue<Tetromino> minos = new Queue<Tetromino>();
+        Tetromino[] template = new Tetromino[7] { Tetromino.I, Tetromino.J, Tetromino.L, Tetromino.O, Tetromino.S, Tetromino.T, Tetromino.Z };
+        Random random;
+        public TetrominoBag(Random random)
+        {
+            this.random = random;
+        }
+        public TetrominoBag()
+        {
+            random = new Random();
+        }
+        public TetrominoBag(int seed)
+        {
+            random = new Random(seed);
+        }
+
+        public Tetromino GetNext()
+        {
+            if(minos.Count == 0)
+                RefillBag();
+            
+            return minos.Dequeue();
+        }
+
+        void RefillBag()
+        {
+            Shuffle();
+            foreach(var mino in template)
+                minos.Enqueue(mino);
+        }
+
+        void Shuffle()
+        {
+            int n = template.Count();
+            while(n > 1)
+            {
+                n--;
+                int k = random.Next(n+1);
+                var value = template[k];
+                template[k] = template[n];
+                template[n] = value;
+            }
+        }
+    }
+}
