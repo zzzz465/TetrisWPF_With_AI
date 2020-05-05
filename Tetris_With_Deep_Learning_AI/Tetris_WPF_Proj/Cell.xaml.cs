e4096e238d61dfab78bd5d286ef98ea961186b09
madeline <zzzz465@naver.com>
2020-05-05 21:48:37 +0900

    update

--- Tetris_With_Deep_Learning_AI/Tetris_WPF_Proj/Cell.xaml.cs
+++ Tetris_With_Deep_Learning_AI/Tetris_WPF_Proj/Cell.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using log4net;
 
 namespace Tetris_WPF_Proj
 {
@@ -21,13 +20,31 @@ namespace Tetris_WPF_Proj
     /// </summary>
     public partial class Cell : UserControl
     {
-        public Style LMinoStyle { get; set; }
-        public Style JMinoStyle { get; set; }
-        public Style ZMinoStyle { get; set; }
-        public Style SMinoStyle { get; set; }
-        public Style TMinoStyle { get; set; }
-        public Style OMinoStyle { get; set; }
-        public Style IMinoStyle { get; set; }
+        #region MinoStyle
+        public Style LMinoStyle { get { return (Style)GetValue(LMinoStyleProperty); } set { SetValue(LMinoStyleProperty, value); } }
+        public Style JMinoStyle { get { return (Style)GetValue(JMinoStyleProperty); } set { SetValue(JMinoStyleProperty, value); } }
+        public Style ZMinoStyle { get { return (Style)GetValue(ZMinoStyleProperty); } set { SetValue(ZMinoStyleProperty, value); } }
+        public Style SMinoStyle { get { return (Style)GetValue(SMinoStyleProperty); } set { SetValue(SMinoStyleProperty, value); } }
+        public Style TMinoStyle { get { return (Style)GetValue(TMinoStyleProperty); } set { SetValue(TMinoStyleProperty, value); } }
+        public Style OMinoStyle { get { return (Style)GetValue(OMinoStyleProperty); } set { SetValue(OMinoStyleProperty, value); } }
+        public Style IMinoStyle { get { return (Style)GetValue(IMinoStyleProperty); } set { SetValue(IMinoStyleProperty, value); } }
+
+        public static readonly DependencyProperty LMinoStyleProperty = DependencyProperty.Register(
+            "LMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty JMinoStyleProperty = DependencyProperty.Register(
+            "JMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty ZMinoStyleProperty = DependencyProperty.Register(
+            "ZMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty SMinoStyleProperty = DependencyProperty.Register(
+            "SMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty TMinoStyleProperty = DependencyProperty.Register(
+            "TMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty OMinoStyleProperty = DependencyProperty.Register(
+            "OMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty IMinoStyleProperty = DependencyProperty.Register(
+            "IMinoStyle", typeof(Style), typeof(Cell), new FrameworkPropertyMetadata());
+        #endregion
+
         Point point;
 
         public Cell()
